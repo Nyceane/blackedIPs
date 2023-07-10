d6ee328ea0665ca97990262491f82dc2d8d9b8c4
Nyceane <nyceane@gmail.com>
2023-07-10 01:05:20 -0700

    trying vite config again

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "/v1": {
         target: "https://server-blackedips.bunnyenv.com",
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
   },
